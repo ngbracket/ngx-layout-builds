00488e84ec328555f32136b39fdba8bafb040487
Duncan Faulkner <info@e-ips.com>
2023-04-18 07:57:39 +0000

    main - 259aeb5 Update label.yml

--- fesm2022/ngbracket-ngx-layout.mjs
+++ fesm2022/ngbracket-ngx-layout.mjs
@@ -82,7 +82,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.0.0-rc.0", ng
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Flex-Layout. */
-const VERSION = new Version('16.0.0-beta.2-b7d4728');
+const VERSION = new Version('16.0.0-beta.2-259aeb5');
 
 /**
  * @license
