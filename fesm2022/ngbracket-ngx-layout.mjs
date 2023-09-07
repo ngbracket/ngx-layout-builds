19352a31b96173f1e1f2921260dee84fdc41c582
Duncan Faulkner <info@e-ips.com>
2023-09-07 19:25:02 +0000

    main - 9e02862 chore(release): v16.1.0

--- fesm2022/ngbracket-ngx-layout.mjs
+++ fesm2022/ngbracket-ngx-layout.mjs
@@ -82,7 +82,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.2.3", ngImpor
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Flex-Layout. */
-const VERSION = new Version('16.1.0-b145043');
+const VERSION = new Version('16.1.0-9e02862');
 
 /**
  * @license
