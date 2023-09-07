5504b296c8cd81f3a0b0f4f6cfab9f187962bd1a
Duncan Faulkner <info@e-ips.com>
2023-09-07 19:15:50 +0000

    main - b145043 chore(version): increament version

--- fesm2022/ngbracket-ngx-layout.mjs
+++ fesm2022/ngbracket-ngx-layout.mjs
@@ -82,8 +82,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.2.3", ngImpor
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Flex-Layout. */
-const VERSION = new Version('16.0.0-765f063');
-
+const VERSION = new Version('16.1.0-b145043');
 
 /**
  * @license
