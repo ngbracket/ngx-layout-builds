fb198889443dbe63a7b5180a3e1324b12052c074
Duncan Faulkner <info@e-ips.com>
2023-11-16 22:17:01 +0000

    17.x.x - a452848 chore(changelog): update changelog

--- fesm2022/ngbracket-ngx-layout.mjs
+++ fesm2022/ngbracket-ngx-layout.mjs
@@ -82,7 +82,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "17.0.3", ngImpor
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Flex-Layout. */
-const VERSION = new Version('17.0.0-5a89707');
+const VERSION = new Version('17.0.0-a452848');
 
 /**
  * @license
