38a71c73f018f9e817cfd854246fd4317179fdf3
Sonu Kapoor <sonukapoor@users.noreply.github.com>
2025-02-02 17:50:25 +0000

    main - ac09a76 chore: fix commands (#65)

--- fesm2022/ngbracket-ngx-layout.mjs
+++ fesm2022/ngbracket-ngx-layout.mjs
@@ -82,7 +82,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "19.0.3", ngImpor
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Flex-Layout. */
-const VERSION = new Version('19.0.0-608c465');
+const VERSION = new Version('19.0.0-ac09a76');
 
 /**
  * @license
