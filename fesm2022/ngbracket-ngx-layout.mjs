a377f4be5ff0a49c80c23e4a219937037478fc4e
Sonu Kapoor <sonukapoor@users.noreply.github.com>
2025-02-03 20:11:34 +0000

    main - 525147d chore: replace yarn commands with pnpm (#67)

--- fesm2022/ngbracket-ngx-layout.mjs
+++ fesm2022/ngbracket-ngx-layout.mjs
@@ -82,7 +82,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "19.0.3", ngImpor
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Flex-Layout. */
-const VERSION = new Version('19.0.0-53a175f');
+const VERSION = new Version('19.0.0-525147d');
 
 /**
  * @license
