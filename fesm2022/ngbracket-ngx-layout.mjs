ca781388639ca20cb91e2a46a0943e99858557ec
Duncan Faulkner <info@e-ips.com>
2025-03-23 17:31:33 +0000

    main - 3bb8649 Merge 1671c2ebf1a4ff0131d04574c606e677f37471fa into d5a496b5521d8adc3b9c555f54a06a2eb2cf3e7b

--- fesm2022/ngbracket-ngx-layout.mjs
+++ fesm2022/ngbracket-ngx-layout.mjs
@@ -65,7 +65,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "19.1.6", ngImpor
                 }] }] });
 
 /** Current version of Angular Flex-Layout. */
-const VERSION = new Version('19.0.0-d5a496b');
+const VERSION = new Version('19.0.0-3bb8649');
 
 /**
  * @module
